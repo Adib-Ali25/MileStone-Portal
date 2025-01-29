785b9e35a2b976959bc39a288d6e7eb67431eba7
Manav Lohabade <manavlohabade26@gmail.com>
2025-01-29 15:22:55 +0530

    Delete src directory

--- src/Routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom';
-import MainRoutes from './MainRoutes';
-
-
-
-const RoutesMS = createBrowserRouter([MainRoutes]);
-
-export default RoutesMS;
- 
